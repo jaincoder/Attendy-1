27be8263e5d18f4a394ef2e487e6a3c370539b7f
Daniel Jing <daniel.jing@berkeley.edu>
2018-09-18 23:20:19 -0700

    Update application.py

--- application.py
+++ application.py
@@ -6,7 +6,7 @@ from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions
 from werkzeug.security import check_password_hash, generate_password_hash
 import os 
-import v0
+from v0 import *
 
 
 from helpers import apology, login_required
