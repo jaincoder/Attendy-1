dc239c5b7696edd73065dac981b9fdc4057b489e
Daniel Jing <daniel.jing@berkeley.edu>
2018-09-18 23:50:39 -0700

    Update application.py

--- application.py
+++ application.py
@@ -74,7 +74,7 @@ def index():
                 attendance = "Absent"
                 return render_template("index.html", password = password, attendance = attendance)
             first_pass = 1
-    passwords = v0.code_generator(6)
+    passwords = code_generator(6)
     password = passwords[0]
     attendance = "Undetermined"
     return render_template("index.html", password = password, attendance = attendance)
