7d0a7875a593aa0a2996a5ba712a76f62710646f
Daniel Jing <daniel.jing@berkeley.edu>
2018-09-19 20:51:25 -0700

    Add files via upload

--- v0.py
+++ v0.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
 import time, datetime, random, string, csv, fileinput#, replit
 
-def export(username, email, id, status):
+def initialize_csv(username, email, id):
     with open('attendance.csv', 'wt', newline='') as csvfile:
         filewriter = csv.writer(csvfile, delimiter=',',
                                 quotechar='|', quoting=csv.QUOTE_MINIMAL)
         filewriter.writerow(["Name", "Email", "SID"])
+        filewriter.writerow([username, email, id])
+    r = csv.reader(open('attendance.csv')) # Here your csv file
+    lines = list(r)
+    for day in range(1, 20):
+            lines[0].append("Week   " + str(day))
+    writer = csv.writer(open('attendance.csv', 'wt', newline=''))
+    writer.writerows(lines)
+
+def export(username, status):
     r = csv.reader(open('attendance.csv')) # Here your csv file
     lines = list(r)
     for l in lines:
@@ -14,86 +23,61 @@ def export(username, email, id, status):
     writer = csv.writer(open('attendance.csv', 'wt', newline=''))
     writer.writerows(lines)
 
-
 def admin(n=3):
     prefix = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(n))
     return prefix
 
-n_admin = 2
-
-# 1 = absent, 2 = present, 3 = restart
-def code_generator(length):
+def code_generator(length, n_admin):
     original = admin(n_admin) + ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(length-n_admin))
     restart = admin(n_admin) + ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(length-n_admin))
     return [original, restart]
 
+# 1 = absent, 2 = present, 3 = restart [logic for functions compare, check1, check2, check3]
+def compare(c_orig, c_input, length):
+    matching = 0
+    if len(c_orig) == len(c_input):
+        for i in range(len(c_orig)):
+            if c_orig[i].lower() == c_input[i].lower():
+                matching += 1
+    else:
+        return 1
+    return matching >= length - 2
+
 def check1(n, k, input, c_orig):
     now = time.time()
-    code = code_generator(n)
-    print (code)
-    ''' check_code = input("What is the code on the screen? ") '''
-    def compare(c_generated, c_input, length):
-        matching = 0
-        for i in range(len(c_generated)):
-            if c_generated[i].lower() == c_input[i].lower():
-                matching += 1
-        return matching >= length - 2
-    if check_code.lower() == code.lower():
+    input, c_orig = str(input), str(c_orig)
+    if input.lower() == c_orig.lower():
         end = time.time()
         if round(end-now) < k:
-            print ("Success! You have been marked as present.")
-            #export(username, email, id, "Present")
             return 2
         else:
-            print ("The attendance window for", str(datetime.datetime.now().strftime("%x")), "has already closed. It will open next on", future_date(7))
-            #export(username, email, id, "Absent")
             return 1
-    elif not compare(code, input, n):
+    elif not compare(c_orig, input, n) or compare(c_orig, input, n) == 1:
         return 1
     else:
         return 3
 
-
 def check2(n, k, input, c_orig):
-        print ("Incorrect Code.")
-        if compare(code, check_code, n) and not attempt:
-            ''' check_code = input("What were the first and last digit of the code? ") '''
-            if input.lower() == code[0].lower() + code[-1].lower():
-                print ("Verified. Generating new code and restarting attendance tracker.")
-                return True
-            else:
-                print ("Too many failed attempts. You have been marked absent.")
-                #export(username, email, id, "Absent")
-                return False
-        else:
-            print ("Too many failed attempts. You have been marked absent.")
-            #export(username, email, id, "Absent")
-            return False
-
-def check3(n, k, input, c_restart):
-    now = time.time()
-    code = code_generator(n)
-    print (code)
-    ''' check_code = input("What is the code on the screen? ") '''
-    def compare(c_generated, c_input, length):
-        matching = 0
-        for i in range(len(c_generated)):
-            if c_generated[i].lower() == c_input[i].lower():
-                matching += 1
-        return matching >= length - 2
-    if check_code.lower() == code.lower():
-        end = time.time()
-        if round(end-now) < k:
-            print ("Success! You have been marked as present.")
-            #export(username, email, id, "Present")
+    input, c_orig = str(input), str(c_orig)
+    if compare(c_orig, input, n):
+        if input.lower() == c_orig[0].lower() + c_orig[-1].lower():
             return True
         else:
-            print ("The attendance window for", str(datetime.datetime.now().strftime("%x")), "has already closed. It will open next on", future_date(7))
-            #export(username, email, id, "Absent")
             return False
     else:
         return False
 
+def check3(n, k, input, c_restart):
+    now = time.time()
+    input, c_restart = str(input), str(c_restart)
+    if input.lower() == c_restart.lower():
+        end = time.time()
+        if round(end-now) < k:
+            return True
+        else:
+            return False
+    else:
+        return False
 
 def future_date(p):
     today = datetime.datetime.now()
@@ -102,95 +86,4 @@ def future_date(p):
     return future.strftime("%m/%d/%Y")
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-
-from tkinter import *
-fields = ('Annual Rate', 'Number of Payments', 'Loan Principle', 'Monthly Payment', 'Remaining Loan')
-
-def monthly_payment(entries):
-   # period rate:
-   r = (float(entries['Annual Rate'].get()) / 100) / 12
-   print("r", r)
-   # principal loan:
-   loan = float(entries['Loan Principle'].get())
-   n =  float(entries['Number of Payments'].get())
-   remaining_loan = float(entries['Remaining Loan'].get())
-   q = (1 + r)** n
-   monthly = r * ( (q * loan - remaining_loan) / ( q - 1 ))
-   monthly = ("%8.2f" % monthly).strip()
-   entries['Monthly Payment'].delete(0,END)
-   entries['Monthly Payment'].insert(0, monthly )
-   print("Monthly Payment: %f" % float(monthly))
-
-def final_balance(entries):
-   # period rate:
-   r = (float(entries['Annual Rate'].get()) / 100) / 12
-   print("r", r)
-   # principal loan:
-   loan = float(entries['Loan Principle'].get())
-   n =  float(entries['Number of Payments'].get())
-   q = (1 + r)** n
-   monthly = float(entries['Monthly Payment'].get())
-   q = (1 + r)** n
-   remaining = q * loan  - ( (q - 1) / r) * monthly
-   remaining = ("%8.2f" % remaining).strip()
-   entries['Remaining Loan'].delete(0,END)
-   entries['Remaining Loan'].insert(0, remaining )
-   print("Remaining Loan: %f" % float(remaining))
-
-def makeform(root, fields):
-   entries = {}
-   for field in fields:
-      row = Frame(root)
-      lab = Label(row, width=22, text=field+": ", anchor='w')
-      ent = Entry(row)
-      ent.insert(0,"0")
-      row.pack(side=TOP, fill=X, padx=5, pady=5)
-      lab.pack(side=LEFT)
-      ent.pack(side=RIGHT, expand=YES, fill=X)
-      entries[field] = ent
-   return entries
-
-if __name__ == '__main__':
-   root = Tk()
-   ents = makeform(root, fields)
-   root.bind('<Return>', (lambda event, e=ents: fetch(e)))
-   b1 = Button(root, text='Final Balance',
-          command=(lambda e=ents: final_balance(e)))
-   b1.pack(side=LEFT, padx=5, pady=5)
-   b2 = Button(root, text='Monthly Payment',
-          command=(lambda e=ents: monthly_payment(e)))
-   b2.pack(side=LEFT, padx=5, pady=5)
-   b3 = Button(root, text='Quit', command=root.quit)
-   b3.pack(side=LEFT, padx=5, pady=5)
-   root.mainloop()
-'''
+print (check1(6, 5, "fqewrs", "fqewrs"))
