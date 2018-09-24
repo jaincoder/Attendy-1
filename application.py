df0ff951a486a619bb7267067ab44aba83327c05
Daniel Jing <daniel.jing@berkeley.edu>
2018-09-24 00:54:06 -0700

    Update application.py

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 from cs50 import SQL
-from flask import Flask, flash, redirect, render_template, request, session, jsonify
+from flask import Flask, flash, redirect, render_template, request, session
 from datetime import datetime, date
 from flask_session import Session
 from tempfile import mkdtemp
